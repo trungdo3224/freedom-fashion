7d347a09355b09f2779158256d5f8c6c225b32ea
momo2912 <trungdo3224@gmail.com>
2022-08-29 17:03:34 +0700

    update state management with redux for cat preview and cat routes

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,19 +1,22 @@
-import { useSelector } from "react-redux";
-import CategoryPreview from "../../components/category-preview/category-preview.component";
-import {categoriesMapSelector} from "../../store/categories/category.selector"
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+
+import { selectCategoriesMap } from '../../store/categories/category.selector';
+
+import CategoryPreview from '../../components/category-preview/category-preview.component';
+
 const CategoriesPreview = () => {
-  
-  const categoriesMap = useSelector(categoriesMapSelector);
+  const categoriesMap = useSelector(selectCategoriesMap);
 
   return (
-    <div className="shop-container">
+    <Fragment>
       {Object.keys(categoriesMap).map((title) => {
-        const  products  = categoriesMap[title];
+        const products = categoriesMap[title];
         return (
           <CategoryPreview key={title} title={title} products={products} />
         );
       })}
-    </div>
+    </Fragment>
   );
 };
 
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,29 +1,32 @@
-import { useParams } from "react-router-dom";
-import {  useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { categoriesMapSelector } from "../../store/categories/category.selector";
-import {CategoryContainer,CategoryTitle } from "./category.styles";
-import ProductCard from "../../components/product-card/product-card.component";
+import { useState, useEffect, Fragment } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
+import ProductCard from '../../components/product-card/product-card.component';
+
+import { selectCategoriesMap } from '../../store/categories/category.selector';
+
+import { CategoryContainer, CategoryTitle } from './category.styles';
 
 const Category = () => {
   const { category } = useParams();
-  const categoriesMap = useSelector(categoriesMapSelector)
+  const categoriesMap = useSelector(selectCategoriesMap);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
     setProducts(categoriesMap[category]);
-  }, [categoriesMap, category]);
+  }, [category, categoriesMap]);
 
   return (
-    <>
-      <CategoryTitle>{category.toLocaleUpperCase()}</CategoryTitle>
+    <Fragment>
+      <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
       <CategoryContainer>
         {products &&
           products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
       </CategoryContainer>
-    </>
+    </Fragment>
   );
 };
 
