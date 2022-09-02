a80861b032966311e6a2621b6cce6ae51e5cbd64
momo2912 <trungdo3224@gmail.com>
2022-09-02 10:21:04 +0700

    remove cart saga file

--- src/store/cart/cart.saga.ts
+++
@@ -1,9 +0,0 @@
-import {all, call, takeLatest, put} from "redux-saga/effects"
-
-export function* onAddItemToCart() {
-    yield* "something"
-} 
-
-export function* cartSaga() {
-    yield all([call(onAddItemToCart)])
-}
