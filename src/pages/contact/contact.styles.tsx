54a8d585c1ad788a749d4d17fecfd05307ab4218
momo2912 <trungdo3224@gmail.com>
2022-09-03 15:03:17 +0700

    init basic contact page

---
+++ src/pages/contact/contact.styles.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ContactContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    
+`
+
+export const ContactInformationContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+    width: 400px;
+    background-color: #cceea1;
+`
